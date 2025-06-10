c5f60203d3c936399e33c7e9ad099078310c92ea
yunchan29 <113772243+yunchan29@users.noreply.github.com>
2025-06-10 17:39:47 +0800

    temporary fix

--- personnelManagement/routes/web.php
+++ personnelManagement/routes/web.php
@@ -27,6 +27,10 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+
+//Temporary
+Route::get('/job/{id}', [JobController::class, 'show'])->name('job.show');
+
 // Registration routes
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
